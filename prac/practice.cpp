81f3773285772e820e5c2c40d0b149339e513ed3
yambaby <lkeijibuendia@gmail.com>
2026-04-01 12:47:41 +0800

    added files

---
+++ prac/practice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int x = 5;
+    int y = 4;
+
+    int product = x * y;
+
+    cout << "The product of" << x << " and "<< y << " is: "<<product;
+    return 0;
+
+}
